c565d5ee4461b23a26fc32f85240226b4a91d365
Acolyte9 <McCradyRT@hendrix.edu>
2026-09-24 15:29:03 -0500

    Implemented functions to interact with the simulation state by changing the terrain, as well as a function to change how the terrain gets interacted with.

--- flutter_application_1/lib/objects/sim_state.dart
+++ flutter_application_1/lib/objects/sim_state.dart
@@ -1,6 +1,12 @@
 import 'dart:math' as math;
 import 'simulation.dart';
 
+enum Tap {
+water,
+solid,
+air,
+}
+
 class GameState {
   GameState(this.rows, this.columns) {
     // snakeLength = math.min(rows, columns) - 5;
@@ -13,6 +19,9 @@ class GameState {
   // late int snakeLength;
   late Fluid fluid;
   // late int iterations;
+  Tap tap = Tap.water;
+
+
 
   // List<math.Point<double>> body = <math.Point<double>>[const math.Point<double>(0, 0)];
   math.Point<double> direction = const math.Point<double>(0, 0);
@@ -29,5 +38,19 @@ class GameState {
     // iterations += 1;
   }
 
-  // void alter_terrain()
+  void changeinteraction(Tap newtype) {
+    tap = newtype;
+  }
+
+  void alterterrain(double x, double y) {
+    if (tap == Tap.water) {
+      fluid.addWater(x, y, 1, 1);
+    }
+    else if (tap == Tap.solid) {
+      fluid.addCircle(x, y, 1, true)
+    }
+    else if (tap == Tap.air) {
+      fluid.addCircle(x, y, 1, false)
+    }
+  }
 }
